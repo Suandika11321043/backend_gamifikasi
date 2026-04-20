395d9ab10ceaeeb0142711de5b1dc81291230b8d
Suandika Napitupulu <out2248@fifgroup.co.id>
2026-04-20 22:16:29 +0700

    update gitignore ,pom and adjust level controller

--- src/main/java/com/example/gamifikasi/dto/LevelDto.java
+++ src/main/java/com/example/gamifikasi/dto/LevelDto.java
@@ -3,12 +3,17 @@ package com.example.gamifikasi.dto;
 import lombok.Data;
 import lombok.AllArgsConstructor;
 import lombok.NoArgsConstructor;
+import jakarta.validation.constraints.NotBlank;
 
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
 public class LevelDto {
     private Long id;
+
+    @NotBlank(message = "nameLevel is required")
     private String nameLevel;
+
+    @NotBlank(message = "description is required")
     private String description;
 }
